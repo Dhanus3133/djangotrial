e55914bd381d3472b55a91f7940f1b26548a6d17
Dhanus3133 <dhanus3133@gmail.com>
2021-01-14 16:43:34 +0530

    new commit

--- trialproject/settings.py
+++ trialproject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '+_rrk!1bc5ni*3uyl1blv1f6*4_r0vgcemn6oyj&ftbs-5u+8*'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['dhanustrial.herokuapp.com']
+ALLOWED_HOSTS = ['dhanustrial.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
